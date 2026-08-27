9cbd0508f1c7846a2f5d44228d0b4fa3f714a3fc
Austin Welsh-Graham <admin@toastysoftware.co.uk>
2026-08-27 02:02:14 +0100

    Add(config): Add the config file with the .env example.

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+
+class Config:
+    APP_ID = os.environ.get("APP_ID")
+    WEBHOOK_SECRET = os.environ.get("WEBHOOK_SECRET")
+    PRIVATE_KEY_PATH = os.environ.get("PRIVATE_KEY_PATH")
+    DB_PATH = os.environ.get("DB_PATH", "/opt/toasty-deployer/deployments.db")
+    REPOS_DIR = os.environ.get("REPOS_DIR", "/repos")
